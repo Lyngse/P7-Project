a9e097aded41acbbd69d92207a7ed07f1442ed32
Lyngse <superlyng@msn.com>
2017-10-16 14:25:19 +0200

    Revert "Cleaning up"
    This reverts commit 75cd89c34983c1964ecc7279b2b94b7a5428d5f7.

---
+++ UnityProj/Assets/scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject die;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - die.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = die.transform.position + offset;
+	}
+}
