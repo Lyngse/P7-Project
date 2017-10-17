b6555606f370c4dbbe1afcd1657987b894533d65
Lyngse <superlyng@msn.com>
2017-10-17 15:33:10 +0200

    Connection with code for angularproject

--- AngularProject/src/app/app.component.ts
+++ AngularProject/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  private headers = new HttpHeaders().set('Content-Type', 'application/json');
 
-  constructor(){
-    console.log("hey");
+  connectionCode: string = "";
+  connectionUrl: string = "http://192.168.0.104:3000";
+
+  constructor(private http: HttpClient){
+    /*console.log("hey");
     let socket = new WebSocket("ws://localhost:5000");
     socket.addEventListener('message', (event) => {
       console.log(event.data);
@@ -17,9 +21,15 @@ export class AppComponent {
     socket.addEventListener('open', () => {
       socket.send("hello from angular");
       
-    });
+    });*/
     
   }
 
-
+  connect() {
+    this.http
+    .post(this.connectionUrl + "/connect", JSON.stringify({code: this.connectionCode}), {
+      headers: this.headers
+    })
+    .subscribe(data => { console.log(data); })
+  }
 }
