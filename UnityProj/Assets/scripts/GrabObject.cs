7dd82e5642e13ac2b0e7a121e325beb0821d5b75
Lyngse <superlyng@msn.com>
2017-10-18 10:49:35 +0200

    Merge with Christian

--- UnityProj/Assets/scripts/GrabObject.cs
+++ UnityProj/Assets/scripts/GrabObject.cs
@@ -7,29 +7,92 @@ public class GrabObject : MonoBehaviour {
     private Vector3 offset;
     private Transform objectTransform;
     private Plane dragPlane = new Plane(Vector3.up, new Vector3(0, 3, 0));
-    private Rigidbody thisBody;
+    public Camera cam;
+    private bool isToggled;    
+    private Touch[] touches;
+    private Dictionary<int, Transform> fingerIDs = new Dictionary<int, Transform>();
 
     private void Start()
     {
-        thisBody = GetComponent<Rigidbody>();
+        this.cam = Camera.main;
     }
 
-    private void OnMouseDown()
+    private void Update()
     {
-        thisBody.useGravity = false;
+        if (!cam.GetComponent<CameraController>().enabled)
+        {
+            touches = Input.touches;
+
+            if(touches.Length >= 1)
+            {
+                for (int i = 0; i < touches.Length; i++)
+                {
+                    RaycastHit hit;
+                    Ray ray = Camera.main.ScreenPointToRay(touches[i].position);
+
+                    if(Physics.Raycast(ray, out hit))
+                    {
+                        if(hit.transform.tag == "Dragable")
+                        {
+                            if (fingerIDs.ContainsKey(touches[i].fingerId))
+                            {
+                                if (!fingerIDs[touches[i].fingerId] == hit.transform)
+                                {
+                                    fingerIDs[touches[i].fingerId] = hit.transform;
+                                }
+                            }
+                            else
+                            {
+                                fingerIDs.Add(touches[i].fingerId, hit.transform);
+                            }
+                        }
+                    }
+
+                    if (touches[i].phase == TouchPhase.Moved)
+                    {
+                        Ray dragplaneRay = Camera.main.ScreenPointToRay(touches[i].position);
+                        float enter = 0;
+                        dragPlane.Raycast(dragplaneRay, out enter);
+                        if (fingerIDs[touches[i].fingerId].tag == "Dragable")
+                        {
+                            fingerIDs[touches[i].fingerId].GetComponent<Rigidbody>().useGravity = false;
+                            fingerIDs[touches[i].fingerId].transform.position = dragplaneRay.GetPoint(enter);
+                        }
+                    }
+                    else if (touches[i].phase == TouchPhase.Canceled || touches[i].phase == TouchPhase.Ended)
+                    {
+                        if (fingerIDs[touches[i].fingerId].tag == "Dragable")
+                        {
+                            fingerIDs[touches[i].fingerId].GetComponent<Rigidbody>().useGravity = true;
+                        }
+                        fingerIDs.Remove(touches[i].fingerId);
+                    }
+                }
+            }            
+        }
     }
 
-    void OnMouseDrag()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        float enter = 0;
-        dragPlane.Raycast(ray, out enter);
-        transform.position = ray.GetPoint(enter);
-        
-    }
+    //private void OnMouseDown()
+    //{
+    //    thisBody.useGravity = false;
+    //}
 
-    private void OnMouseUp()
-    {
-        thisBody.useGravity = true;
-    }
+    //void OnMouseDrag()
+    //{
+    //    if (isToggled)
+    //    {
+    //        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //float enter = 0;
+            //dragPlane.Raycast(ray, out enter);
+            //transform.position = ray.GetPoint(enter);
+    //    }       
+    //}
+
+    //private void OnMouseUp()
+    //{
+    //    if (isToggled)
+    //    {
+    //        thisBody.useGravity = true;
+    //    }        
+    //}
 }
