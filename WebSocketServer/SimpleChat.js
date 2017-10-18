d6ae4783ddc3e691b786687e9ff87089fe571677
Andreas Smed <9049518+smed95@users.noreply.github.com>
2017-10-18 12:15:47 +0200

    Add server ip/code exchange
    Add additional js packages

--- WebSocketServer/SimpleChat.js
+++ WebSocketServer/SimpleChat.js
@@ -1,52 +1,119 @@
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 5000 });
-console.log('Server started');
+var ip = require('ip');
+var http = require('http');
+var querystring = require('querystring');
+var request = require('request');
+const serverPort = 5000;
+const wss = new WebSocket.Server({ port: serverPort });
+console.log('Server started on: ' + ip.address() + ' on port: ' + serverPort);
+
 
 // Broadcast a message to all clients
 wss.broadcast = function broadcast(data) {
   wss.clients.forEach(function each(client) {
-   // if (client.readyState === WebSocket.OPEN) {
-      client.send(data);
-   // }
+    // if (client.readyState === WebSocket.OPEN) {
+    client.send(data);
+    // }
   });
 };
 
 // This function adds the remote address to each connection for later use
 wss.on('connection', function connection(ws, req) {
+  console.log(req.connection.remoteAddress + " joined the chat\n");
+  wss.broadcast(req.connection.remoteAddress);
+  ws.remoteAddress = req.connection.remoteAddress;
+});
+
+/* This function adds the remote address to each connection for later use
+wss.on('connection', function connection(ws, req) {
   console.log(req.connection.remoteAddress  + " joined the chat\n");
   wss.broadcast(req.connection.remoteAddress);
+  
   wss.clients.forEach(function each(client) {
     if (client === ws) {
       client.remoteAddress = req.connection.remoteAddress;
     }
   });
 });
+*/
 
 // This function forwards messages to a specified client
 // Needs error handling
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(data) {
 
+    var message = null;
     console.log("Debug1: " + data);
-    var message = JSON.parse(data);
-    console.log("Debug2: " + message.type + ": " + message.message);
-    
-    if(message.type === 'forward'){
-      console.log("Debug3: " );
-      
-      wss.clients.forEach(function each(client) {
-        console.log("Debug4: " + client.remoteAddress);
-        
-        if(message.reciever == client.remoteAddress){
-          client.send(data);
-          console.log("Debug5: Send to" + client.remoteAddress);
-        }
-      
-      });
+
+    try {
+      message = JSON.parse(data);
+    }
+    catch (e) {
+      alert(e); // error in the above string (in this case, yes)!
     }
 
+
+    console.log("Debug2: " + message.type + ": " + message.message);
+    if (message === null) {
+      if (message.type === 'forward') {
+        console.log("Debug3: ");
+
+        wss.clients.forEach(function each(client) {
+          console.log("Debug4: " + client.remoteAddress);
+
+          if (message.reciever == client.remoteAddress) {
+            client.send(data);
+            console.log("Debug5: Send to" + client.remoteAddress);
+          }
+        });
+      }
+    }
   });
 });
+
+// This function sends all the connected clients' ips to the new client
+wss.on('connection', function connection(ws) {
+  wss.clients.forEach(function each(client) {
+    ws.send(client.remoteAddress);
+  });
+});
+
+
+
+
+
+
+
+
+
+var serverCode;
+
+request.post(
+  'https://p7-webserver.herokuapp.com/api/host',
+  //'http://localhost:3000/api/host',
+  { json: { ip: ip.address() } },
+  function (error, response, body) {
+    //if (!error && response.statusCode == 200) {
+    console.log(body);
+    console.log(body.hasOwnProperty('data'));
+
+    if (body.success) {
+      serverCode = body.data.code;
+      console.log(serverCode);
+    }
+    else{
+      
+    }
+
+
+  }
+);
+
+
+
+
+
+
 
 /* This function broadcasts all messages to the other clients
 wss.on('connection', function connection(ws) {
